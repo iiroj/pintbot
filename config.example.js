6177695e280a524dfa3c90c14d9fd5b05d68a6f6
Iiro Jäppinen <iiro@jappinen.fi>
2016-03-28 10:50:50 +0300

    remove foursquare from config

--- config.example.js
+++ config.example.js
@@ -1,15 +1,10 @@
 var telegram = {
 	"url": "https://URL-WITHOUT-SLASH",
 	"token": "TOKEN"
-}, foursquare = {
-	"clientId": "CLIENT-ID",
-	"clientSecret": "CLIENT-SECRET"
 }, google = {
   "apiKey": "API-KEY"
 }
 
 exports.telegramUrl = telegram.url
 exports.telegramToken = telegram.token
-exports.foursquareClientId = foursquare.clientId
-exports.foursquareClientSecret = foursquare.clientSecret
 exports.googleApiKey = google.apiKey
