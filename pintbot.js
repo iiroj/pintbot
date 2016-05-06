0647988b4aa236fabc9741adb2a36fac2eb43685
Iiro Jäppinen <iiro@jappinen.fi>
2016-05-06 09:47:35 +0300

    log prod vs dev into console when starting

--- pintbot.js
+++ pintbot.js
@@ -13,9 +13,11 @@ const recents      = Dirty(__dirname + "/recents.json");
 if(process.env.IS_PROD == true) {
   const pintbot    = new Bot(config.telegramToken);
   pintbot.setWebHook(config.telegramUrl + "/" + config.telegramToken);
+  console.log("Pint Bot is running in production mode")
 }
 else {
   const pintbot    = new Bot(config.telegramToken, { polling: true });
+  console.log("Pint Bot is running in development mode")
 }
 
 // Suggest pubs based on the user's location
